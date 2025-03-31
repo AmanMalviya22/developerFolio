8e44c5407b7de0223f4b1822e3da226f9231b6b3
Aman Malviya <94959490+AmanMalviya22@users.noreply.github.com>
2025-03-31 16:43:50 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -156,7 +156,7 @@ const workExperiences = {
     },
 
   {
-  role: "Full Stack Developer | Mar 2024 – Feb 2025",
+  role: "Java Developer | Mar 2024 – Sep 2024",
   company: "BeeperMD",
   companylogo: require("./assets/images/beepermd.png"),
   date: "",
@@ -170,22 +170,22 @@ const workExperiences = {
     </ul>
   )
  }
-// ,
-// {
-//   role: "DevOps Engineer | Oct 2024 – Feb 2025",
-//   company: "VivaOps",
-//   companylogo: require("./assets/images/vivaops.avif"),
-//   date: "",
-//   desc: (
-//     <ul>
-//       <li>Deployed a 3-tier microservice on an EKS cluster with Docker and Kubernetes.</li>
-//       <li>Created CI/CD pipelines using Jenkins for automated builds, testing, and deployments.</li>
-//       <li>Used AWS (VPN, EC2, S3, RDS) to improve system reliability and reduce downtime by 5%.</li>
-//       <li>Automated infrastructure with Terraform and Ansible, cutting deployment time by 10%.</li>
-//       <li>Tech Stack: AWS, GitLab, Linux, Docker, Kubernetes, Jenkins, Jira, Git, GitHub.</li>
-//     </ul>
-//   )
-// }
+,
+{
+  role: "DevOps Engineer | Oct 2024 – Feb 2025",
+  company: "VivaOps",
+  companylogo: require("./assets/images/vivaops.avif"),
+  date: "",
+  desc: (
+    <ul>
+      <li>Deployed a 3-tier microservice on an EKS cluster with Docker and Kubernetes.</li>
+      <li>Created CI/CD pipelines using Jenkins for automated builds, testing, and deployments.</li>
+      <li>Used AWS (VPN, EC2, S3, RDS) to improve system reliability and reduce downtime by 5%.</li>
+      <li>Automated infrastructure with Terraform and Ansible, cutting deployment time by 10%.</li>
+      <li>Tech Stack: AWS, GitLab, Linux, Docker, Kubernetes, Jenkins, Jira, Git, GitHub.</li>
+    </ul>
+  )
+}
    
 
 
